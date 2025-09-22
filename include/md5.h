b00abef4ce3ed29deded1159f5d1444f051a7d53
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2025-09-22 22:40:10 +0300

    refactor: update everything to follow Google C++ style guide

--- include/md5.h
+++ include/md5.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef MD5_LIB_INCLUDE_MD5_H_
+#define MD5_LIB_INCLUDE_MD5_H_
 
 #include <istream>
 #include <string>
@@ -17,7 +18,7 @@ namespace md5_lib {
  * @throw std::ios_base::failure on stream reading errors if the stream's
  *        exception mask is set to throw on `badbit`.
  */
-[[nodiscard]] std::string calculate_md5(std::istream& stream);
+[[nodiscard]] std::string CalculateMD5(std::istream& stream);
 
 /**
  * @brief Calculates the MD5 hash of a contiguous block of memory.
@@ -26,12 +27,15 @@ namespace md5_lib {
  * a std::string or std::vector's data.
  *
  * @param data A non-null pointer to the beginning of the data block.
- *             The type is `const unsigned char*` as it is the convention.
+ *             The type is `const unsigned char*` as it is the conventional
+ *             type in C++ for representing raw, untyped byte data.
  * @param size The size of the data block in bytes.
  * @return A 32-character lowercase hexadecimal string representing the MD5
  * hash.
  */
-[[nodiscard]] std::string calculate_md5(const unsigned char* data,
-                                        std::size_t size);
+[[nodiscard]] std::string CalculateMD5(const unsigned char* data,
+                                       std::size_t size);
 
 }  // namespace md5_lib
+
+#endif  // MD5_LIB_INCLUDE_MD5_H_
--- tests/md5_test.cpp
+++ tests/md5_test.cpp
@@ -1,7 +1,6 @@
 #include "md5.h"
 
-#include <gtest/gtest.h>
-
+#include <chrono>
 #include <cstdio>
 #include <filesystem>
 #include <fstream>
@@ -9,83 +8,77 @@
 #include <string>
 #include <vector>
 
-class RFC1321TestVectors : public ::testing::Test {};
+#include "gtest/gtest.h"
 
 void RunTest(const std::string& input, const std::string& expected_hash) {
-  // 1. Test the memory-based API
-  const std::string hash_mem = md5_lib::calculate_md5(
+  const std::string hash_mem = md5_lib::CalculateMD5(
       reinterpret_cast<const unsigned char*>(input.data()), input.size());
   EXPECT_EQ(hash_mem, expected_hash);
 
-  // 2. Test the stream-based API
   std::stringstream ss(input);
-  const std::string hash_stream = md5_lib::calculate_md5(ss);
+  const std::string hash_stream = md5_lib::CalculateMD5(ss);
   EXPECT_EQ(hash_stream, expected_hash);
 }
 
-TEST_F(RFC1321TestVectors, EmptyString) {
+TEST(RFC1321TestVectors, EmptyString) {
   RunTest("", "d41d8cd98f00b204e9800998ecf8427e");
 }
 
-TEST_F(RFC1321TestVectors, a) {
+TEST(RFC1321TestVectors, a) {
   RunTest("a", "0cc175b9c0f1b6a831c399e269772661");
 }
 
-TEST_F(RFC1321TestVectors, abc) {
+TEST(RFC1321TestVectors, abc) {
   RunTest("abc", "900150983cd24fb0d6963f7d28e17f72");
 }
 
-TEST_F(RFC1321TestVectors, MessageDigest) {
+TEST(RFC1321TestVectors, MessageDigest) {
   RunTest("message digest", "f96b697d7cb7938d525a2f31aaf161d0");
 }
 
-TEST_F(RFC1321TestVectors, LowercaseAlphabet) {
+TEST(RFC1321TestVectors, LowercaseAlphabet) {
   RunTest("abcdefghijklmnopqrstuvwxyz", "c3fcd3d76192e4007dfb496cca67e13b");
 }
 
-TEST_F(RFC1321TestVectors, Alphanumeric) {
+TEST(RFC1321TestVectors, Alphanumeric) {
   RunTest("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
           "d174ab98d277d9f5a5611c2c9f419d9f");
 }
 
-TEST_F(RFC1321TestVectors, LongStringOfNumbers) {
+TEST(RFC1321TestVectors, LongStringOfNumbers) {
   RunTest(
       "123456789012345678901234567890123456789012345678901234567890123456789012"
       "34567890",
       "57edf4a22be3c955ac49da2e2107b67a");
 }
 
-class BoundaryConditionTests : public ::testing::Test {};
-
-TEST_F(BoundaryConditionTests, LessThanOneBlock_PaddingBoundary_55_Bytes) {
+TEST(BoundaryConditionTests, LessThanOneBlock_PaddingBoundary_55_Bytes) {
   std::string input(55, 'a');
   RunTest(input, "ef1772b6dff9a122358552954ad0df65");
 }
 
-TEST_F(BoundaryConditionTests, LessThanOneBlock_PaddingBoundary_56_Bytes) {
+TEST(BoundaryConditionTests, LessThanOneBlock_PaddingBoundary_56_Bytes) {
   std::string input(56, 'a');
   RunTest(input, "3b0c8ac703f828b04c6c197006d17218");
 }
 
-TEST_F(BoundaryConditionTests, LessThanOneBlock_PaddingBoundary_63_Bytes) {
+TEST(BoundaryConditionTests, LessThanOneBlock_PaddingBoundary_63_Bytes) {
   std::string input(63, 'a');
   RunTest(input, "b06521f39153d618550606be297466d5");
 }
 
-TEST_F(BoundaryConditionTests, ExactlyOneBlock_64_Bytes) {
+TEST(BoundaryConditionTests, ExactlyOneBlock_64_Bytes) {
   std::string input(64, 'a');
   RunTest(input, "014842d480b571495a4a0363793f7367");
 }
 
-TEST_F(BoundaryConditionTests, JustOverOneBlock_65_Bytes) {
+TEST(BoundaryConditionTests, JustOverOneBlock_65_Bytes) {
   std::string input(65, 'a');
   RunTest(input, "c743a45e0d2e6a95cb859adae0248435");
 }
 
-class APITest : public ::testing::Test {};
-
-TEST_F(APITest, ThrowsOnNullptr) {
-  EXPECT_THROW(static_cast<void>(md5_lib::calculate_md5(nullptr, 10)),
+TEST(APITest, ThrowsOnNullptr) {
+  EXPECT_THROW(static_cast<void>(md5_lib::CalculateMD5(nullptr, 10)),
                std::invalid_argument);
 }
 
@@ -110,14 +103,14 @@ class LargeInputTest : public ::testing::Test {
     }
 
     // Write 10 MiB of data
-    constexpr std::size_t k_file_size = 10 * 1024 * 1024;
-    constexpr char k_char_to_write = 'M';
-    const std::vector<char> buffer(4096, k_char_to_write);
+    constexpr std::size_t kFileSize = 10 * 1024 * 1024;
+    constexpr char kCharToWrite = 'M';
+    const std::vector<char> buffer(4096, kCharToWrite);
 
-    for (std::size_t bytes_written = 0; bytes_written < k_file_size;
+    for (std::size_t bytes_written = 0; bytes_written < kFileSize;
          bytes_written += buffer.size()) {
       const std::size_t bytes_to_write =
-          std::min(buffer.size(), k_file_size - bytes_written);
+          std::min(buffer.size(), kFileSize - bytes_written);
       outfile.write(buffer.data(), bytes_to_write);
     }
   }
@@ -137,7 +130,7 @@ TEST_F(LargeInputTest, StreamMatchesMemoryForLargeFile) {
   ASSERT_TRUE(infile.is_open())
       << "Failed to open temporary file for reading: " << temp_filepath_;
 
-  const std::string stream_hash = md5_lib::calculate_md5(infile);
+  const std::string stream_hash = md5_lib::CalculateMD5(infile);
 
   EXPECT_EQ(stream_hash, expected_hash);
 }
